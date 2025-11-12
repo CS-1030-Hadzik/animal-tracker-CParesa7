acac01a948f2397a8027cbd5d48e437d77f03f2c
Cameron Paresa <Cameronparesa@mail.weber.edu>
2025-11-12 10:35:11 -0800

    Made final adjustments

--- animal.py
+++ animal.py
@@ -4,15 +4,16 @@ class Animal:
     """
     # Class-level attribute
     kingdom = "Animalia"
-    # TODO create a class-level attribute that is a list of all the Animal objects
-    
+    # Class-level attribute that is a list of all the Animal objects
+    all_animals = []
     def __init__(self, name, species):
         self.name = name
         self.species = species
-
+        Animal.all_animals.append(self)
     # method is a way to do something with the class
     def speak(self):
         print("The animal makes a noise.")
 
+    # overriding the method
     def __str__(self):
-        return f"Kingdom: {self.kingdom}, Name: {self.name} Species: {self.species}"
+        return f"Kingdom: '{self.kingdom}', Name: '{self.name}', Species: '{self.species}'"
--- main.py
+++ main.py
@@ -8,14 +8,16 @@ if __name__ == "__main__":
     fido = Dog("Fido", "Canine", "Bulldog")
 
     print (kitty)
-    print (fido)
     kitty.speak()
+
+    print (fido)
     fido.speak()
-    # TODO: Print the Animal instance
-    # TODO: Call the method to make a generic animal sound
 
-    # TODO: Create an instance of the Dog class
-    # TODO: Print the Dog instance
-    # TODO: Call the method to make the dog-specific sound
+    # Print out all the animals
+    print("\nAll traked animals:")
+    for animal in Animal.all_animals:
+        print(f"  - {animal}")
 
-    # TODO print out all the animals
+
+# List Animal.all_animals
+# object animal
